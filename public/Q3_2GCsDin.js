2b742d702c25ef6d24c79bb8d96010ac28836906
LourdyDin <b2029lcpdin@pshs.edu.ph>
2026-02-03 19:17:44 +0800

    Eighteenth Commit

--- public/Q3_2GCsDin.js
+++ public/Q3_2GCsDin.js
@@ -1,13 +1,16 @@
-// read from the localStorage saved as a string - to see if there are anything saved on the users coomputer
-let acctString = localStorage.getItem("accounts")
-if (!acctString) { accountList = {} } // initialize the variable to contain the list of accounts object
-else accountList = JSON.parse(acctString) // converts string into the correct data type in this case object
+// read from the localStorage saved as a string - to see if there are any signups saved on the user's computer
+let signupString = localStorage.getItem("signups");
+if (!signupString) { 
+    signups = []; // initialize the variable to contain the list of signups as an empty array
+} else { 
+    signups = JSON.parse(signupString); // converts string into the correct data type in this case array of objects
+}
 
-const form = document.getElementById("dForm"); // get the HTML form from q3ge2Mendoza.html
+const form = document.getElementById("dForm"); // get the HTML form from Q3_2GCsDin.html
 
 // event handler on the submit button instead of onsubmit on the button itself
 form.addEventListener("submit", function(e) { // assign an event handler of submit to the form
-    //e.preventDefault(); // prevent page reload because forms gets submitted
+    e.preventDefault(); // prevent page reload because forms get submitted
 
     if (confirm("Sure You Want To Save Your Work?")) {   
         // use a predefined class to create an object of data
@@ -15,24 +18,22 @@ form.addEventListener("submit", function(e) { // assign an event handler of subm
 
         // Convert to object
         const obj = Object.fromEntries(data.entries()); // get all the data from the form
-        // place the object inside the accountList
-        // accountList is an object containing other objects with username as the key
-        accountList[obj.uname] = {};
-        for (let key in obj) { // go through the properties of the object and create another account
-            if (key != "uname") { 
-                accountList[obj.uname][key] = obj[key];
-            }
-        }
-        
-        console.log(accountList) // to check all the account information if it will be saved correctly
-        acctString = JSON.stringify(accountList) // convert object into string, as a requirement of localStorage
-        localStorage.setItem("accounts", acctString) // save on the user's computer
-        form.submit();
+
+        // push the new signup object into the array
+        signups.push(obj);
+
+        console.log(signups); // to check all the signup information if it will be saved correctly
+
+        signupString = JSON.stringify(signups); // convert array into string, as a requirement of localStorage
+        localStorage.setItem("signups", signupString); // save on the user's computer
+
+        alert("Signup saved successfully!");
+        form.reset(); // clear the form after saving
     }
-  });
+});
 
 // event handler for the reset button instead of onreset on the button itself
-form.addEventListener("reset", function(e) { // 
+form.addEventListener("reset", function(e) { 
   // Ask for confirmation before clearing
   if (!confirm("Sure you want to clear your data?")) {
     e.preventDefault(); // cancel the reset if user clicks "Cancel"
@@ -41,5 +42,6 @@ form.addEventListener("reset", function(e) { //
 
 // called when user is on the input field
 function BlurFunction() {
+    // example: reset background color of an input field
     document.getElementById("myInput").style.backgroundColor = "";
-  }
+}
